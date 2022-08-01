aae8fc2c7517eb7ad4f058037ff6b597ba761867
Corey Scanlan <bideinsilence@gmail.com>
2022-08-01 13:33:51 -0700

    Add
    New solution for confirm-the-ending

--- confirm-the-ending/confirmEnding.js
+++ confirm-the-ending/confirmEnding.js
@@ -7,21 +7,26 @@
 
 
 function confirmEnding(str, target) {
-    // With a for loop:
-    // Loop through the number of characters in the target string
-    // i starts at 0 for string index
-    for (let i = 0; i < target.length; i++) {
-        // Index str starting at a distance from the end of str equivilant to
-        // the start of target, (str.length - target.length + i)
-        // Return false if the value at str index doesn't match the value at
-        // target index
-        if ( str[str.length - target.length + i] !== target[i] ) {
-            return false;
-        }
-    }
-    // If all characters match,
-    return true;
+    //// With a for loop:
+    //// Loop through the number of characters in the target string
+    //// i starts at 0 for string index
+    //for (let i = 0; i < target.length; i++) {
+    //    // Index str starting at a distance from the end of str equivilant to
+    //    // the start of target, (str.length - target.length + i)
+    //    // Return false if the value at str index doesn't match the value at
+    //    // target index
+    //    if ( str[str.length - target.length + i] !== target[i] ) {
+    //        return false;
+    //    }
+    //}
+    //// If all characters match,
+    //return true;
+    // With substring():
+    // Returns true if the string (substring) starting at the index from the end
+    // of str (str.length) minus the length of the target string (target.length)
+    // is the same as the target string, false if not
+    return str.substring(str.length - target.length) === target;
 }
 
-console.log(confirmEnding("Bastian", "n"));
+confirmEnding("Bastian", "n");
 
