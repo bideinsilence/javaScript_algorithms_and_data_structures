411d40d0800efd272f9ca239816187883ea7a033
Corey Scanlan <bideinsilence@gmail.com>
2022-08-01 22:04:28 -0700

    Add
    New solution for title-case-a-sentence

--- title-case-a-sentence/titleCase.js
+++ title-case-a-sentence/titleCase.js
@@ -6,21 +6,28 @@
 
 
 function titleCase(str) {
-    // Break str into an array of individual words
-    const words = str.split(" ");
-    // New array for capitalized words
-    const capitalized = [];
-    // Loop through each word in array
-    for (let word of words ) {
-        // New word with all letters in word lowercase
-        const lowerWord = word.toLowerCase();
-        // Capitalize the first letter of word (string), concatinate the rest of
-        // the word (string), and add capitalized word (string) to capitalized
-        // array
-        capitalized.push(lowerWord.charAt(0).toUpperCase() + lowerWord.slice(1));
-    }
-    // Return new string with words from array joined by an empty space
-    return capitalized.join(" ");
+    //// With a loop:
+    //// Make all letters in str lower case
+    //const lowerCase = str.toLowerCase();
+    //// Break str into an array of individual words
+    //const words = lowerCase.split(" ");
+    //// New array for capitalized words
+    //const capitalized = [];
+    //// Loop through each word in array
+    //for (let word of words ) {
+    //    // Capitalize the first letter of word (string), concatinate the rest of
+    //    // the word (string), and add capitalized word (string) to capitalized
+    //    // array
+    //    capitalized.push(word.charAt(0).toUpperCase() + word.slice(1));
+    //}
+    //// Return new string with words from array joined by an empty space
+    //return capitalized.join(" ");
+    // With replace() and regex:
+    return str
+        // Make all letters lower case
+        .toLowerCase()
+        // Match any character following a whitespace or matching the first character of the whole string
+        .replace(/(^|\s)\S/g, (firstLetter) => firstLetter.toUpperCase());
 }
 
 console.log(titleCase("I'm a little tea pot"));
