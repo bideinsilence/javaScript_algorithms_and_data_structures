fdab5a8e753c4a784e556006d20f5856f557560f
Corey Scanlan <bideinsilence@gmail.com>
2022-08-02 13:21:49 -0700

    Add
    New solution for falsy-bouncer

--- falsy-bouncer/bouncer.js
+++ falsy-bouncer/bouncer.js
@@ -6,8 +6,20 @@
 
 
 function bouncer(arr) {
-  // Return only elements, filter(), that convert to true, Boolean()
-  return arr.filter(element => Boolean(element));
+    //// With a for loop:
+    //// New array to return
+    //const newArray = [];
+    //// Iterate over each value in the array
+    //for (let element of arr) {
+    //// Use the if statement to check if the current element is truthy or falsy
+    //// If the element is truthy, add it to the new array
+    //    if ( element ) newArray.push(element);
+    //}
+    //return newArray;
+    // With filter() and Boolean():
+    // Return only elements, filter(), that convert to true, Boolean()
+    // return arr.filter(element => Boolean(element));
+    return arr.filter(Boolean);
 }
 
 console.log(bouncer([7, "ate", "", false, 9]));
