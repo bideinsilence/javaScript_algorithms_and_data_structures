b2a660ebb93624370ecc3d219ea147e9c870d49f
Corey Scanlan <bideinsilence@gmail.com>
2022-08-06 13:23:35 -0700

    Add
    New solution for sum-all-numbers-in-a-range

--- sum-all-numbers-in-a-range/sumAll.js
+++ sum-all-numbers-in-a-range/sumAll.js
@@ -24,21 +24,36 @@ function sumAll(arr) {
     //        sum += i;
     //    }
     //}
+    //// Return accumulator
     //return sum;
     //
-    // Cleaner, using Math.min() and Math.max():
+    // Cleaner, using Math.min(), Math.max(), and the spread operator:
     // Accumulator to return
     let sum = 0;
     // Find the smaller of the two numbers, Math.min()
-    const min = Math.min(arr[0], arr[1]);
+    const min = Math.min(...arr);
     // Find the larger of the two numbers, Math.max()
-    const max = Math.max(arr[0], arr[1]);
+    const max = Math.max(...arr);
     // Interate through each increasing value in range, inclusive
     for (let i = min; i <= max; i++) {
         // Add that value to the accumulator
         sum += i;
     }
+    // Return accumulator
     return sum;
+    //
+    //// With the formula for finding the sum of consecutive numbers and
+    //// Math.abs():
+    //// (first number + last number)(n / 2) = sum, where n is the number of
+    //// integers
+    //const firstNumber = arr[0];
+    //const lastNumber = arr[1];
+    //// Find the number of integers in the range; the absolute value,
+    //// Math.abs(), of the first number subtracted from the last, + 1 to
+    //// include the first number in the total
+    //const numberOfIntegers = Math.abs(lastNumber - firstNumber) + 1;
+    //// Return the summation formula
+    //return (firstNumber + lastNumber) * (numberOfIntegers / 2);
 }
 
 sumAll([1, 4]);
