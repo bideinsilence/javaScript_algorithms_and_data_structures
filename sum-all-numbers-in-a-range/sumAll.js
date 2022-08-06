5edb598035201bc54ae69f279c03af3468a40d90
Corey Scanlan <bideinsilence@gmail.com>
2022-08-05 17:16:06 -0700

    Add
    New solution for sum-all-numbers-in-a-range

--- sum-all-numbers-in-a-range/sumAll.js
+++ sum-all-numbers-in-a-range/sumAll.js
@@ -7,21 +7,36 @@
 
 
 function sumAll(arr) {
+    //// Original solution:
+    //// Accumulator to return
+    //let sum = 0;
+    //// If first value is smaller than the second
+    //if ( arr[0] < arr[1] ) {
+    //    // Iterate through each increasing value in range, inclusive
+    //    for (let i = arr[0]; i <= arr[1]; i++) {
+    //        // Add the value to the accumulator
+    //        sum += i;
+    //    }
+    //} else {
+    //    // Iterate through each decreasing value in range, inclusive
+    //    for (let i = arr[0]; i >= arr[1]; i--) {
+    //        // Add that value to the accumulator
+    //        sum += i;
+    //    }
+    //}
+    //return sum;
+    //
+    // Cleaner, using Math.min() and Math.max():
     // Accumulator to return
     let sum = 0;
-    // If first value is smaller than the second
-    if ( arr[0] < arr[1] ) {
-        // Loop through each increasing value in range, inclusive
-        for (let i = arr[0]; i <= arr[1]; i++) {
-            // Add value to accumulator
-            sum += i;
-        }
-    } else {
-        // Loop through each decreasing value in range, inclusive
-        for (let i = arr[0]; i >= arr[1]; i--) {
-            // Add value to accumulator
-            sum += i;
-        }
+    // Find the smaller of the two numbers, Math.min()
+    const min = Math.min(arr[0], arr[1]);
+    // Find the larger of the two numbers, Math.max()
+    const max = Math.max(arr[0], arr[1]);
+    // Interate through each increasing value in range, inclusive
+    for (let i = min; i <= max; i++) {
+        // Add that value to the accumulator
+        sum += i;
     }
     return sum;
 }
